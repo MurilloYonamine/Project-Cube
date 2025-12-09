803532c70f6110ffe6d480cdee6fa93645d88fbf
Murillo Gomes Yonamine <murillo.gyonamine@gmail.com>
2025-12-09 13:27:23 -0300

    refactor: now player can use all 4 direction inputs

--- Assets/Scripts/Player/Components/PlayerMovement.cs
+++ Assets/Scripts/Player/Components/PlayerMovement.cs
@@ -10,6 +10,7 @@ namespace PROJECT_CUBE.PLAYER.COMPONENTS {
         [Header("Movement Settings")]
         [SerializeField] private float _movementSpeed = 2f;
         [SerializeField] private float _jumpForce = 5f;
+        private Vector2 _movementInput;
 
         [Header("Ground Detection")]
         [SerializeField] private float _groundDragDistance = 0.5f;
@@ -17,6 +18,7 @@ namespace PROJECT_CUBE.PLAYER.COMPONENTS {
 
         public override void AwakeComponent() {
             _rigidbody = _playerController.GetComponent<Rigidbody>();
+            LogMovementInput(Vector2.zero);
         }
 
         public override void OnEnableComponent() {
@@ -30,6 +32,8 @@ namespace PROJECT_CUBE.PLAYER.COMPONENTS {
             AutoMovement();
         }
         private void HandleMovement(Vector2 movementInput) {
+            _movementInput = movementInput;
+
             LogMovementInput(movementInput);
 
             // Pulo para cima
@@ -48,8 +52,9 @@ namespace PROJECT_CUBE.PLAYER.COMPONENTS {
             _rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
         }
         private void AutoMovement() {
-            Vector2 movement = new Vector2(1, 0) * _movementSpeed;
-            _rigidbody.linearVelocity = new Vector2(movement.x, _rigidbody.linearVelocity.y);
+            float direction = _movementInput.x != 0 ? _movementInput.x : 1f;
+            float movement = Mathf.Sign(direction) * _movementSpeed;
+            _rigidbody.linearVelocity = new Vector3(movement, _rigidbody.linearVelocity.y, _rigidbody.linearVelocity.z);
         }
         private void CheckGroundStatus() {
             Vector3 raycastOrigin = _rigidbody.position + Vector3.down * 0.5f;
@@ -70,18 +75,13 @@ namespace PROJECT_CUBE.PLAYER.COMPONENTS {
         }
         private void LogMovementInput(Vector2 movementInput) {
             string message = string.Empty;
-            switch (movementInput.y) {
-                case > 0:
-                    message = $"Input: Para cima ({movementInput.y})";
-                    break;
-                case < 0:
-                    message = $"Input: Para baixo ({movementInput.y})";
-                    break;
-                default:
-                    message = $"Input: Ignorado";
-                    break;
-            }
-
+            message = movementInput switch {
+                Vector2 input when input.x > 0 => "Movendo para a direita",
+                Vector2 input when input.x < 0 => "Movendo para a esquerda",
+                Vector2 input when input.y > 0 => "Pulando",
+                Vector2 input when input.y < 0 => "Caindo mais rápido",
+                _ => "Nenhum Input de Movimento",
+            };
             PlayerDebugManager.Instance.AddLine($"{message}", nameof(PlayerMovement));
         }
     }
