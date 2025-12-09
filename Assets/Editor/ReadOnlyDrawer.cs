000b0f72fd0430f6dd1f7cbf39b71d6b331a913f
Murillo Gomes Yonamine <murillo.gyonamine@gmail.com>
2025-12-09 04:07:15 -0300

    feat: add ReadOnlyAttribute and ReadOnlyDrawer for custom property handling in the editor

---
+++ Assets/Editor/ReadOnlyDrawer.cs
@@ -0,0 +1,12 @@
+using UnityEditor;
+using UnityEngine;
+using PROJECT_CUBE.ATTRIBUTES;
+
+[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+public class ReadOnlyDrawer : PropertyDrawer {
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+        GUI.enabled = false;
+        EditorGUI.PropertyField(position, property, label, true);
+        GUI.enabled = true;
+    }
+}
---
+++ Assets/Scripts/Attributes/ReadOnlyAttribute.cs
@@ -0,0 +1,5 @@
+using UnityEngine;
+
+namespace PROJECT_CUBE.ATTRIBUTES {
+    public class ReadOnlyAttribute : PropertyAttribute { }
+}
